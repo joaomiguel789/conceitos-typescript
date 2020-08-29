201a49d5e87f2752a8f3788278031580fc17c9f3
Miguel <joao.martins789@gmail.com>
2020-08-28 22:13:10 -0300

    tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,21 @@
 import { Request, Response} from 'express'
- 
+import createUser  from './services/CreateUser'
+
+// string, number, boolean, object, array
+// interfaces
+
 export function helloWorld(request: Request, response: Response) {
+    const user = createUser({
+        name: 'Miguel',
+        email: 'miguel@miguel',
+        password: '12345',
+        techs: [
+            'Node.js', 
+            'ReactJS', 
+            'React Native',
+            { title: 'Javascript', experience: 100}
+        ]
+    })
+
     return response.json({ message: 'Hello World'})
 }
